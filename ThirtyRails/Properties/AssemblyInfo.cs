c3ccc9a81f00f24dcae475487ec33768675dcd11
Mihi <devdumbblind@googlemail.com>
2017-02-15 13:00:04 +0800

    initial commit

---
+++ ThirtyRails/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("ThirtyRails")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MadCowDevelopment")]
+[assembly: AssemblyProduct("ThirtyRails")]
+[assembly: AssemblyCopyright("Copyright MadCowDevelopment © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
