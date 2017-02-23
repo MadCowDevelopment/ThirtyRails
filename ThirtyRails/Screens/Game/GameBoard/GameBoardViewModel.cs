6c482d337aab74f8420ce060ec98e93a339000f2
Mihi <devdumbblind@googlemail.com>
2017-02-23 12:07:10 +0800

    added basic GameBoard

---
+++ ThirtyRails/Screens/Game/GameBoard/GameBoardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.Composition;
+using ThirtyRails.Shell;
+
+namespace ThirtyRails.Screens.Game.GameBoard
+{
+    [Export(typeof(GameBoardViewModel))]
+    public class GameBoardViewModel : ViewModel
+    {
+    }
+}
--- ThirtyRails/Screens/Game/GameViewModel.cs
+++ ThirtyRails/Screens/Game/GameViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Composition;
 using Caliburn.Micro;
+using ThirtyRails.Screens.Game.GameBoard;
 using ThirtyRails.Shell;
 
 namespace ThirtyRails.Screens.Game
@@ -7,9 +8,16 @@ namespace ThirtyRails.Screens.Game
     [Export]
     public class GameViewModel : Screen, IViewModel
     {
+        [ImportingConstructor]
+        public GameViewModel(GameBoardViewModel gameBoard)
+        {
+            GameBoard = gameBoard;
+        }
+
+        public IViewModel GameBoard { get; private set; }
+
         public void Initialize()
         {
-            
         }
     }
 }
