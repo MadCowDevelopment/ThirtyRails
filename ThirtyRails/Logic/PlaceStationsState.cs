a043ba3cefd2b70f8ee882a5624cf4a85591e337
Mihi <devdumbblind@googlemail.com>
2017-02-24 16:33:37 +0800

    added station numbers

--- ThirtyRails/Logic/PlaceStationsState.cs
+++ ThirtyRails/Logic/PlaceStationsState.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using ThirtyRails.Utils;
 
@@ -18,17 +19,19 @@ namespace ThirtyRails.Logic
         {
             if (!tile.IsValidTarget) return;
 
-            tile.IsStation = !tile.IsStation;
-
+            var tilesInSameLine = GetTilesInSameLine(tile).Where(p => p != tile);
             if (tile.IsStation)
             {
-                var tilesInSameLine =
-                    (tile.IsHorizontal ? Map.GetTilesInRow(tile.Y) : Map.GetTilesInColumn(tile.X))
-                    .OfType<StationTile>()
-                    .Where(p => p != tile);
-                tilesInSameLine.ForEach(p => p.IsStation = false);
+                tile.Number = 0;
+                tilesInSameLine.ForEach(p => p.IsValidTarget = true);
+            }
+            else
+            {
+                tilesInSameLine.ForEach(p => p.IsValidTarget = false);
             }
 
+            tile.IsStation = !tile.IsStation;
+            
             if (Map.StationTiles.Count(p => p.IsStation) == 4)
             {
                 Map.StationTiles.ForEach(p =>
@@ -43,12 +46,37 @@ namespace ThirtyRails.Logic
         protected override void OnEnter(StationTile tile)
         {
             if (!tile.IsValidTarget) return;
+            if (tile.IsStation) return;
+
             tile.PreviewStation = true;
+
+            var firstFreeNumber = 0;
+            var stations = Map.StationTiles.Where(p => p.IsStation).ToList();
+            for (var i = 1; i <= 4; i++)
+            {
+                if (stations.All(p => p.Number != i))
+                {
+                    firstFreeNumber = i;
+                    break;
+                }
+            }
+
+            tile.Number = firstFreeNumber;
         }
 
         protected override void OnLeave(StationTile tile)
         {
             tile.PreviewStation = false;
+            if(!tile.IsStation) tile.Number = 0;
+        }
+
+        private IEnumerable<StationTile> GetTilesInSameLine(StationTile tile)
+        {
+            var tilesInSameLine =
+                (tile.IsHorizontal ? Map.GetTilesInRow(tile.Y) : Map.GetTilesInColumn(tile.X))
+                .OfType<StationTile>()
+                .Where(p => p != tile);
+            return tilesInSameLine;
         }
     }
 }
--- ThirtyRails/Logic/StationTile.cs
+++ ThirtyRails/Logic/StationTile.cs
@@ -6,8 +6,11 @@
         {
         }
 
+        public bool ShowStation => IsStation || PreviewStation;
         public bool IsStation { get; set; }
         public bool PreviewStation { get; set; }
         public bool IsHorizontal => Y == 0 || Y == 7;
+
+        public int Number { get; set; }
     }
 }
